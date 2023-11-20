67de8ca860fa147b2c82c0b9cfa6cd6769e898ee
sai-sreekhar <sai.sreekhar@gmail.com>
2023-11-20 22:15:53 +0530

    working code commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import Result from "./components/Result";
+import Upload from "./components/Upload";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Upload />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
